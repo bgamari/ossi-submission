1bfe88e72e8f461dee7518785428086218abcec4
Ben Gamari <bgamari.foss@gmail.com>
2013-11-14 11:16:10 -0500

    Add note

--- talk/ExportLayers.hs
+++ talk/ExportLayers.hs
@@ -7,7 +7,7 @@ import qualified Text.XML as Xml
 import Text.XML.Lens
 import Data.Default
 import Data.Monoid ((<>))
-import Control.Error
+import Control.Error hiding (note)
 import Control.Monad.Trans
 import Control.Monad.Trans.Writer
 import Control.Monad (when, forM)
@@ -28,6 +28,9 @@ slide title figure = tell [FigureSlide title figure id]
 tslide :: String -> String -> Writer [Slide] ()
 tslide title text = tell [TextSlide title text]
 
+note :: Text -> Writer [Slide] ()
+note _ = return ()
+
 title = "An open-source toolchain for fluorescence spectroscopy"
 
 main = runEitherT $ do
@@ -46,100 +49,107 @@ main = runEitherT $ do
 slides :: [Slide] 
 slides = execWriter $ do
      -- introduce single molecule measurement
-     -- We want to be able to see individual molecules, like this one here
+     note "We want to be able to see individual molecules, like this one here"
      fslide "A single-molecule" "fret.svg"
        $ showOnlyLayers ["molecule", "molecule-label"]
-     -- however molecules are quite small. How do we see them?
+     
+     note "however molecules are quite small. How do we see them?"
      fslide "A single-molecule" "fret.svg"
        $ showOnlyLayers ["molecule", "molecule-label", "scale-bar"]
 
-     -- One powerful approach is to attach fluorescent probe
+     note "One powerful approach is to attach fluorescent probe"
      fslide "Seeing a single molecule" "fret.svg"
        $ showOnlyLayers ["molecule", "donor", "fluorophore-label"]
-     -- this is a molecule which we can shine light on to excite
+
+     note "this is a molecule which we can shine light on to excite"
      fslide "Seeing a single molecule" "fret.svg"
        $ showOnlyLayers ["molecule", "donor", "fluorophore-label", "donor-exc-photon"]
      fslide "Seeing a single molecule" "fret.svg"
        $ showOnlyLayers ["molecule", "excited-donor"]
-     -- later the molecule will decay, resulting in light which we can detect
+     
+     
+     note "later the molecule will decay, resulting in light which we can detect"
      fslide "Seeing a single molecule" "fret.svg"
        $ showOnlyLayers ["molecule", "donor", "donor-em-photon"]
-     -- Even a single fluorescent probe is sufficient to measure
-     -- molecular size, characterize reaction kinetics, and more
 
-     -- Let's say, though, that we want to do something a bit more sophisticated:
-     -- measure the distance between two points on our molecule
+     note "Even a single fluorescent probe is sufficient to measure molecular size, characterize reaction kinetics, and more"
+
+     note "but let's say, though, that we want to do something a bit more sophisticated:"
+     note "measure the distance between two points on our molecule"
      fslide "Measuring a single molecule" "fret.svg"
        $ showOnlyLayers ["molecule", "donor", "molecule-distance"]
-     -- we can do this by adding a second probe of a different color to our molecule
+
+     note "we can do this by adding a second probe of a different color to our molecule"
      fslide "Measuring an intramolecular distance" "fret.svg"
        $ showOnlyLayers ["molecule", "donor", "donor-exc-photon", "acceptor"]
-     -- now we can excite one of our dyes
+
+     note "now we can excite one of our dyes"
      fslide "Measuring an intramolecular distance" "fret.svg"
        $ showOnlyLayers ["molecule", "excited-donor", "acceptor"]
-     -- Sometimes the dye will transfer
-     -- (donate) its energy to the other by a process known as Fӧrster
-     -- transfer. After this a photon will be emitted by the acceptor dye.
+
+     note "Sometimes the dye will transfer (donate) its energy to the other by a process known as Fӧrster transfer"
+     note "After this a photon will be emitted by the acceptor dye."
      fslide "Measuring an intramolecular distance" "fret.svg"
        $ showOnlyLayers ["molecule", "donor", "donor-label", "acceptor", "acceptor-label", "energy-transfer"]
-     -- The probability of energy transfer is higher when the dyes are close together
-     -- giving us a measurement of distance
+     
+     note "The probability of energy transfer is higher when the dyes are close together giving us a measurement of distance"
      fslide "Measuring an intramolecular distance" "fret.svg"
        $ showOnlyLayers ["molecule", "donor", "excited-acceptor"]
      fslide "Measuring an intramolecular distance" "fret.svg"
        $ showOnlyLayers ["molecule", "donor", "acceptor-em-photon", "acceptor"]
 
-     -- FRET efficiency depends upon distance, stoichastic
+     note "FRET efficiency depends upon distance, stoichastic"
      slide "Measuring an intramolecular distance" "conformations.svg"
 
-     -- Typically these experiments are done with an apparatus like this with the
-     -- molecule diffusing in solution. We get short bursts of fluorescence
+     note "Typically these experiments are done with an apparatus like this"
+     note "Labelled molecule diffusing in solution"
+     note "We get short bursts of fluorescence"
      fslide "A fluorescence experiment" "fret-setup.svg"
        $ showOnlyLayers ["background", "labels", "hardware", "fret-inset", "fret-labels", "detector-label", "legend"]
      
-     -- we want to go from photons
+     note "we want to go from photons"
      fslide "An inverse problem" "photons-to.svg"
        $ showOnlyLayers ["photons"]
-     -- to distance
+     note "to distance"
      fslide "An inverse problem" "photons-to.svg"
        $ showOnlyLayers ["photons", "arrow", "distribution", "distance-label"]
-     -- or barring that, something related to distance
+     note "or barring that, something related to distance"
      fslide "An inverse problem" "photons-to.svg"
        $ showOnlyLayers ["photons", "arrow", "distribution", "fret-eff-label"]
-     -- but once we have this arrow, we've solved our problem
+     note "but once we have this arrow, we've solved our problem"
 
-     -- Great, so we can measure distances, right?
+     note "Great, so we can measure distances, right?"
      fslide "A fluorescence experiment" "fret-setup.svg"
        $ showOnlyLayers ["background", "hardware", "fret-inset"]
-     -- The devil is in the details: our data is flawed
+     note "The devil is in the details: our data is flawed"
      fslide "Errors abound" "fret-setup.svg"
        $ showOnlyLayers ["background", "fret-inset", "fret-artifacts"]
 
-     -- We need to correct for these effects
-     -- We can start by collecting more data
-     fslide "Corrections" "fret-setup.svg"
+     note "We need to correct for these effects"
+     
+     note "fluorescence spectroscopic tools unique view on geometry of single molecule systems"
+     note "but quantitative analysis of the experimental results is challenging"
+     note "My submission consists of a set of open-source tools built on open platforms for the collection, manipulation, analysis of fluorescence spectroscopy data."
+
+     note "We offer an open-source photon timestamping instrument built on off-the-shelf hardware."
+     note "As opposed to most commercial, instruments, the device is flexible enough to enable FRET variants such as alternating excitation which enables more sophisticated correction for the artifacts discussed above"
+     note "Additionally, the hardware is orders of magnitude cheaper than equally capable commercial options."
+     fslide "Contribution: acquisition hardware" "fret-setup.svg"
        $ highlightLayers 0.2 ["hardware"]
        . showOnlyLayers ["background", "hardware", "fret-inset"]
      
-     -- While fluorescence spectroscopic tools such as FRET provide a 
-     -- unique view on geometry of single molecule systems,
-     -- quantitative analysis of the experimental results is challenging. My
-     -- submission consists of a set of open-source tools built on open
-     -- platforms for the collection, manipulation, analysis of
-     -- fluorescence spectroscopy data.
-     
-     -- Photon timestamping instrument built on off-the-shelf hardware enables
-     -- novel experimental designs
-     fslide "Data collection" "" id
-     
-     -- Acquisition software can be easily integrated into existing
-     -- systems, enabling scanning and other applications
-     fslide "" "" id
+     note "Also provide easy-to-use acquisition software."
+     note "Can be easily integrated into existing systems, enabling scanning and other applications"
+     --fslide "" "" id
 
-     -- Data analysis tools provide both easy-to-use command line interfaces 
-     -- as well as libraries for incorporation into existing analysis pipelines
+     note "Data analysis tools provide both easy-to-use command line interfaces as well as libraries for incorporation into existing analysis pipelines"
+     note "Analysis tools work not only with data from our own instrument, but also with commercial formats"
+     --slide "" "" id
      
-     -- Statistical inference tools enable novel analyses
+     -- 
+     -- burst detection
+     
+     note "Statistical inference tools enable novel analyses"
      
 
 type LayerLabel = Text
